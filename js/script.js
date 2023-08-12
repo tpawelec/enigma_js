5fa22fdd08f79db981cb02bc20e170500b2715ee
Tomasz Pawelec <tpawelec@outlook.com>
2023-08-12 18:37:30 +0200

    plugboard highlighting ongoin

--- js/script.js
+++ js/script.js
@@ -12,9 +12,16 @@ const plugboard = document.querySelector("#enigmaPlugboard");
 
 const alphabet = ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Z', 'X', 'C', 'V', 'B', 'N', 'M'];
 const keyCodes = [81, 87, 69, 82, 84, 89, 85, 73, 79, 80, 65, 83, 68, 70, 71, 72, 74, 75, 76, 90, 88, 67, 86, 66, 78, 77]
-    /*
-        Generating keyboard layout
-    */
+
+/*
+    Plugboard pairs
+*/
+
+let plugboardPairs = [];
+
+/*
+    Generating keyboard layout
+*/
 let row1 = document.createElement('div');
 let row2 = document.createElement('div');
 let row3 = document.createElement('div');
@@ -26,7 +33,8 @@ row3.classList.add('keyboard__row');
 alphabet.forEach((value, index) => {
     let letter = document.createElement('p');
     //letter.setAttribute('id', 'letter' + value);
-    letter.setAttribute('data-keycode', keyCodes[index])
+    letter.setAttribute('data-keycode', "Key" + value)
+    letter.setAttribute('data-clicked', "off")
     letter.innerHTML = value;
     if (index < 9) {
         row1.appendChild(letter);
@@ -46,6 +54,94 @@ plugboard.appendChild(row2.cloneNode(true));
 rotors.appendChild(row3);
 keyboard.appendChild(row3.cloneNode(true));
 plugboard.appendChild(row3.cloneNode(true));
+
+
+/*
+    Plugboard highlight
+*/
+let plugBoardColors = [
+    '#4e1b6f',
+    '#a8b499',
+    '#2da31a',
+    '#ca6690',
+    '#7d8b96',
+    '#ff6f61',
+    '#007acc',
+    '#6bbb69',
+    '#ffd700',
+    '#c72138',
+    '#e06236',
+    '#304c7a',
+    '#b3df7f'
+]
+
+let usedColors = [];
+let plugBoardKeys = plugboard.querySelectorAll(".main__plugboard p");
+let pairCounter = 1;
+let colorCounter = 0;
+let wasSomethingRemoved = false;
+plugBoardKeys.forEach((el) => {
+    el.addEventListener('mousedown', (elem) => {
+        if(el.getAttribute('data-clicked') == 'off') {
+            el.setAttribute('data-clicked', 'on');
+            if(pairCounter == 1) {
+                plugboardPairs.push([el.innerHTML])
+                pairCounter++;
+                el.style.setProperty('--plugColor', plugBoardColors[plugboardPairs.length - 1]);
+            } else {
+                plugboardPairs[plugboardPairs.length - 1].push(el.innerHTML);
+                pairCounter--;
+                if(wasSomethingRemoved) {
+                    el.style.setProperty('--plugColor', plugBoardColors[plugboardPairs.length - 2]);
+                } else {
+                    el.style.setProperty('--plugColor', plugBoardColors[plugboardPairs.length - 1]);
+                    wasSomethingRemoved = false;
+                }
+                usedColors.push(plugBoardColors[plugBoardColors.length - 1]);
+                plugBoardColors.pop();
+            }
+            
+            
+            
+        } else {
+            el.style.setProperty('--plugColor', '#000000');
+            el.setAttribute('data-clicked', 'off');
+            let indexes = pairElement(plugboardPairs, el.innerHTML);
+            /*
+                indexes[1] index in plugboardPairs
+                indexes[0] index in array in plugboardPairs
+
+            */
+            console.log(plugboardPairs[indexes[1]][(indexes[0]+1)%2])
+            let secondLetter = plugboard.querySelector('.main__plugboard p[data-keycode="Key' + plugboardPairs[indexes[1]][(indexes[0]+1)%2] + '"]');
+            let color = secondLetter.style.getPropertyValue('--plugColor');
+            
+            plugBoardColors.push(color);
+            usedColors.splice(usedColors.indexOf(color), 1);
+
+            secondLetter.style.setProperty('--plugColor', '#000000');
+            secondLetter.setAttribute('data-clicked', 'off');
+            plugboardPairs.splice(pairElement(plugboardPairs, el.innerHTML)[1] , 1);
+            wasSomethingRemoved = true;
+            
+        }
+
+        
+    })
+})
+
+
+let pairElement = (arr, key) => {
+    let indexesArray = [];
+    arr.forEach((el, i) => {
+        if(el.indexOf(key) > -1) {
+            indexesArray.push(el.indexOf(key));
+            indexesArray.push(i)
+        }
+    })
+
+    return indexesArray;
+}
 
 /*
     Light ciphered keyboard
@@ -81,9 +177,10 @@ keyboardLetters.forEach((el) => {
 
 
 document.addEventListener('keydown', (event) => {
+    console.log(event)
     let code = event.code || event.keyCode;
     console.log(code)
-    let letter = document.querySelector('.main__keyboard p[data-keycode=' + code);
+    let letter = document.querySelector('.main__keyboard p[data-keycode="' + code + '"]');
     console.log(letter)
 
 })
